6bc155951b6608291ce8ac302036296bfa648125
am121-dwc <150588833+am121-dwc@users.noreply.github.com>
2024-05-29 22:34:07 +0900

    save

---
+++ Variable.java
@@ -0,0 +1,9 @@
+class Variable{
+  public static void main(String[] args) {
+    int month;
+    month = 5;
+    int day = 29;
+    System.out.println("今日は、");
+    System.out.println(month + "月" + day + "日です！");
+  }
+}
